3953adef9f03eb1b99716da924b447ac117d9c4a
bernardoaraujor <bernardoaraujor@gmail.com>
2014-11-13 17:04:15 -0200

    Update test2d.py

--- test2d.py
+++ test2d.py
@@ -1,15 +1,9 @@
 
 import numpy as np
-import scipy as sp
 import scipy.signal as sg
 import simpleSTC as sstc
 import compiSTAC as ci
-import matplotlib
-
-try:
-    import matplotlib.pylab as plt
-except ImportError:
-    pass
+import matplotlib.pylab as plt
 
 nt = 20 # number of temporal elements of filter
 tvec = np.vstack(np.arange(-nt+1., 1)) # time vector
@@ -19,15 +13,24 @@ filt2 = np.vstack((np.diff(filt1, 1, 0), 0)) # 2nd filter
 filt2 = filt2 - filt1*np.sum(filt1*filt2) # orthogonalize to 1st filter
 filt2 = (filt2)/np.linalg.norm(filt2) # normalize
 
-slen = 10000 # Stimulus length
+x = np.arange(0, 20)
+filt3 = (x-10)**2
+filt3[0:10] = 0
+filt3 = (filt3)/np.linalg.norm(filt3)
+
+filt1 = np.reshape(filt1, (nt, 1))
+filt2 = np.reshape(filt2, (nt, 1))
+filt3 = np.reshape(filt3, (nt, 1))
+
+slen = 100000 # Stimulus length
 #mat = sp.io.loadmat('C:/Users/bernardo/Documents/MATLAB/USP/iSTAC/stimtest.mat')
 #Stim = mat['Stim']
 Stim = plt.randn(slen, 1.)
 RefreshRate = 100. # refresh rate
 
 DC = (0.75, .5)
-linresp = np.hstack((sg.convolve2d(np.concatenate((np.zeros((len(filt1)-1, Stim.shape[1])), Stim), 0), np.rot90(filt1, 2), 'valid')+.75, sg.convolve2d(np.concatenate((np.zeros((len(filt2)-1, Stim.shape[1])), Stim), 0), np.rot90(filt2, 2), 'valid')+.5)) # filter output
-r = 10*linresp[:,0]**2 + 8*linresp[:,1]**2      # instantaneous spike rate
+linresp = np.hstack((sg.convolve2d(np.concatenate((np.zeros((len(filt1)-1, Stim.shape[1])), Stim), 0), np.rot90(filt1, 2), 'valid')+.75, sg.convolve2d(np.concatenate((np.zeros((len(filt2)-1, Stim.shape[1])), Stim), 0), np.rot90(filt2, 2), 'valid')+.5, sg.convolve2d(np.concatenate((np.zeros((len(filt3)-1, Stim.shape[1])), Stim), 0), np.rot90(filt3, 2), 'valid')+.5)) # filter output
+r = 10*linresp[:,0]**2 + 10*linresp[:,1]**2 + 5*linresp[:,2]**2     # instantaneous spike rate
 r = np.vstack(r)
 #spikes = mat['spikes']
 spikes = np.random.poisson(r/RefreshRate)   # generate spikes
@@ -47,21 +50,33 @@ fig1 = plt.figure()
 plt.plot(np.arange(1, ndims+1), KLcontributed, 'o--')
 plt.title('KL contribution')
 plt.xlabel('subspace dimensionality')
-plt.savefig('plots/KLcontribution')
+plt.show()
+#plt.savefig('plots/KLcontribution')
 
 fig2 = plt.figure()
-plt.plot(tvec, np.hstack((filt1, np.dot(u[:,(0, 1)], np.dot(u[:,(0, 1)].T, filt1)), np.dot(vecs[:,(0, 1)], np.dot(vecs[:,(0, 1)].T, filt1)))))
-plt.title('Reconstruction of 1st filter')
-plt.savefig('plots/reconst_1st_filt.png')
+f1, = plt.plot(tvec, filt1)
+stc1, = plt.plot(tvec, np.dot(u[:,(0, 1)], np.dot(u[:,(0, 1)].T, filt1)))
+istac1, = plt.plot(tvec, vecs[:, 0])
+plt.legend([f1, stc1, istac1], ['true k', 'STC', 'iSTAC'], loc = 2)
+plt.title('1st Kernel')
+plt.show()
+#plt.savefig('plots/reconst_1st_filt.png')
 
 fig3 = plt.figure()
-plt.plot(tvec, np.hstack((filt2, np.dot(u[:,(0, 1)], np.dot(u[:,(0, 1)].T, filt2)), np.dot(vecs[:,(0, 1)], np.dot(vecs[:,(0, 1)].T, filt2)))))
-plt.title('Reconstruction of 2nd filter')
-plt.savefig('plots/reconst_2nd_filt.png')
+f2, = plt.plot(tvec, filt2)
+stc2, = plt.plot(tvec, np.dot(u[:,(0, 1)], np.dot(u[:,(0, 1)].T, filt2)))
+istac2, = plt.plot(tvec, vecs[:, 1])
+plt.legend([f2, stc2, istac2], ['true k', 'STC', 'iSTAC'], loc = 2)
+plt.title('2nd Kernel')
+plt.show()
+#plt.savefig('plots/reconst_2nd_filt.png')
 
-fig4 = plt.figure()
-plt.plot(tvec, np.hstack((np.vstack(vecs[:, 0]), np.vstack(vecs[:, 1]))))
-plt.title('iSTAC filters')
-plt.savefig('plots/iSTAC_filters')
+fig32 = plt.figure()
+f3, = plt.plot(tvec, filt3)
+stc2, = plt.plot(tvec, np.dot(u[:,(0, 1)], np.dot(u[:,(0, 1)].T, filt2)))
+istac2, = plt.plot(tvec, vecs[:, 2])
+plt.legend([f2, stc2, istac2], ['true k', 'STC', 'iSTAC'], loc = 2)
+plt.title('3rd Kernel')
+plt.show()
+#plt.savefig('plots/reconst_2nd_filt.png')
 
-a = 2
